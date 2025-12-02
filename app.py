ded1ae6be819c3d39343f1108c827ebf9699ed5b
Shashank Chavan <163658669+YONE-Shashank@users.noreply.github.com>
2025-12-02 12:51:27 +0530

    Commited by Shashank

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-# Addition
-sum_result = 10 + 5
-print(f"10 + 5 = {sum_result}") # Output: 15
-
-# Subtraction
-diff_result = 20 - 7
-print(f"20 - 7 = {diff_result}") # Output: 13
-
-# Multiplication
-prod_result = 6 * 4
-print(f"6 * 4 = {prod_result}") # Output: 24
-
+############################################
 # Division (results in a float)
 div_result = 100 / 8
 print(f"100 / 8 = {div_result}") # Output: 12.5
